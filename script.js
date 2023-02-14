19c79fcc59fb8a0202466762469c780c7b0eb201
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-02-14 17:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+'use strict';
+const arr = document.querySelector('.textInput').value;
+arr.sort();
+let sum = 0;
+
+const btn = document.querySelector('.submitInput');
+
+//function
+
+function showResult() {
+	for(let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	document.querySelector('#resutl').textContent = sum;
+}
+
+btn.addEventListener('click', showResult);
